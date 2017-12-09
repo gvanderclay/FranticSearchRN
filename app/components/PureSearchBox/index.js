20fb53cfd5e4336fe9ad1ee449836c96054572b0
Gage Vander Clay <gvanderclay@gmail.com>
2017-12-09 13:33:38 -0500

    Start testing pure search box

--- app/components/PureSearchBox/index.js
+++ app/components/PureSearchBox/index.js
@@ -1,9 +1,23 @@
 import * as React from "react";
 import { TextInput } from "react-native";
 import style from "./style";
+import PropTypes from "prop-types";
 
 export class PureSearchBox extends React.Component {
   render() {
-    return <TextInput placeholder="Search" style={style.main} />;
+    const { setSearch, searchTerm } = this.props;
+    return (
+      <TextInput
+        placeholder="Search"
+        style={style.main}
+        onChangeText={setSearch}
+        value={searchTerm}
+      />
+    );
   }
 }
+
+PureSearchBox.PropTypes = {
+  setSearch: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired
+};
